379992cb6eb4bc47d72c09c6c6f78ba8d990105b
tmtri3102 <tmtri310251@gmail.com>
2024-11-29 10:16:32 +0700

    Thuc hanh MVC

---
+++ src/main/java/com/example/customermanager/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.example.customermanager.service;
+
+import com.example.customermanager.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
